5f0a0985f0d407a473610255f20024e141bb220b
DevOps76-2024 <subbubyns7799@gmail.com>
2024-01-14 14:59:34 +0530

    new data updated

--- 04.program1.sh
+++ 04.program1.sh
@@ -4,4 +4,4 @@ PERSON2=Naresh
 echo "$PERSON1: Hi $PERSON2, Good morning"
 echo "$PERSION2: very good morning $PERSION1"
 echo "$PERSON1: what ia the plan for the day $PERSON2"
-echo "$PERSION2: i do not have any kind of plans $PERSION2"
+echo "$PERSION2: i do not have any kind of plans $PERSION1"
