d37b742ddbea03d7e2e154aad7b10c7f6abb9823
DevOps76-2024 <subbubyns7799@gmail.com>
2024-01-14 15:05:38 +0530

    new script updated recently

--- 04.program1.sh
+++ 04.program1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 PERSON1=Giri
 PERSON2=Naresh
+
 echo "$PERSON1: Hi $PERSON2, Good morning"
-echo "$PERSION2: very good morning $PERSION1"
-echo "$PERSON1: what ia the plan for the day $PERSON2"
-echo "$PERSION2: i do not have any kind of plans $PERSION1"
+echo "$PERSION2: very good morning $PERSION1 ,Thank you for the message"
+echo "$PERSON1: what ia the plan for the day $PERSON2 ,Shall we plan something"
+echo "$PERSION2: i do not have any kind of plans $PERSION1 ,lets go for a movie"
