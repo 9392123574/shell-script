0070da95b83035aac3cf14f262e31c15e80cff36
DevOps76-2024 <subbubyns7799@gmail.com>
2024-01-14 11:06:56 +0530

    new script

---
+++ 04.program1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Giri: Hi Naresh, Good morning"
+echo "Naresh: very good morning Giri"
+echo "Giri: what ia the plan for the day Naresh"
+echo "Naresh: i do not have any kind of plans Giri"
