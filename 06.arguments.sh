e81afab15c8c810737301441f41653e26f192e8f
DevOps76-2024 <subbubyns7799@gmail.com>
2024-01-14 15:38:05 +0530

    arguments have UPDATED

---
+++ 06.arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good morning"
+echo "$PERSION2: very good morning $PERSION1 ,Thank you for the message"
+echo "$PERSON1: what ia the plan for the day $PERSON2 ,Shall we plan something"
+echo "$PERSION2: i do not have any kind of plans $PERSION1 ,lets go for a movie"
